756f57dde2fd27f9104faff153beb16ce387eb5f
Predrag Carapic <64416113+Chaka15@users.noreply.github.com>
2021-03-12 17:11:30 +0100

    fixing errors

--- ecommerce/src/components/UI/BackToTop/BackToTop.js
+++ ecommerce/src/components/UI/BackToTop/BackToTop.js
@@ -2,22 +2,20 @@ import React from "react";
 
 import BackToTop from "react-easy-back-to-top";
 
-const BackToTopButton = () => {
-  return (
-    <BackToTop
-      icon="fa fa-arrow-up"
-      backgroundColor="#f8f9fa"
-      color="#212529"
-      fontSize="14px"
-      position={{ right: "1%", bottom: "1%" }}
-      hover={{ backgroundColor: "red", color: "black" }}
-      transition="all 0.5s"
-      showOnDistance={1100}
-      scrollBehavior="smooth"
-      borderRadius={24}
-      opacity="1"
-    />
-  );
-};
+const BackToTopButton = () => (
+  <BackToTop
+    icon="fa fa-arrow-up"
+    backgroundColor="#f8f9fa"
+    color="#212529"
+    fontSize="14px"
+    position={{ right: "1%", bottom: "1%" }}
+    hover={{ backgroundColor: "red", color: "black" }}
+    transition="all 0.5s"
+    showOnDistance={1100}
+    scrollBehavior="smooth"
+    borderRadius={24}
+    opacity="1"
+  />
+);
 
 export default BackToTopButton;
