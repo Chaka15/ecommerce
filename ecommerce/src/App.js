0e999ac40611a698b673c28896bbf6ba4b7eff93
Predrag Carapic <64416113+Chaka15@users.noreply.github.com>
2021-03-12 18:36:02 +0100

    .

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import FavoritesPage from "./components/FavoritesPage/FavoritesPage";
 import InformationPage from "./components/InformationPage/InformationPage";
@@ -21,15 +21,17 @@ function App() {
 
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" component={HomePage} exact />
-        <Route path="/saved" component={SavedSection} exact />
-        <Route path="/logout" component={Logout} exact />
-        <Route path="/favorites" component={FavoritesPage} exact />
-        <Route path="/auth" component={Auth} exact />
-        <Route path="/:id" component={InformationPage} exact />
-        <Route component={ErrorPage} />
-      </Switch>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/saved" component={SavedSection} exact />
+          <Route path="/logout" component={Logout} exact />
+          <Route path="/favorites" component={FavoritesPage} exact />
+          <Route path="/auth" component={Auth} exact />
+          <Route path="/:id" component={InformationPage} exact />
+          <Route component={ErrorPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
